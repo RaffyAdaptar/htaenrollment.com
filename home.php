669f0b5693222083d60a776600627e7844c3f50a
RaffyAdaptar <165022905+RaffyAdaptar@users.noreply.github.com>
2024-03-26 19:31:30 +0800

    Add files via upload
    ICT BARACHIEL

---
+++ home.php
@@ -0,0 +1,81 @@
+ <!-- Map Column -->
+ <div class="row">
+            <div class="col-md-4">
+                <img width="100%" src="img/school_seal_100x100.pn">
+            </div>
+            <!-- Contact Details Column -->
+            <div class="col-md-8">
+                <h3>Contact Details</h3>
+                <p>
+                    Holy Trinity Academy of Loay 
+                <br>Poblacion Ubos Loay Bohol<br>
+                </p>
+                <p><i class="fa fa-phone"></i> 
+                    <abbr title="Phone">P</abbr>: (083) 228-9722</p>
+                <p><i class="fa fa-envelope-o"></i> 
+                    <abbr title="Email">Email</abbr>: <a href="mailto:name@example.com">Admission@HTA.com</a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> 
+                    <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 5:00 PM</p>
+                <ul class="list-unstyled list-inline list-social-icons">
+                    <li>
+                        <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
+                    </li>
+                </ul>
+            </div>
+</div>
+    <!-- /.row -->
+          
+                     <br/>
+            <div class="row">
+                 
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                       <!--  <div class="panel-heading">
+                            <h3><b>  Mission and Vision</b>
+                           </h3>
+                        </div> -->
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <p>
+                               <h1> VISION</h1>
+
+                                    test test test
+
+                                     
+
+                                   <h1>  MISSION</h1>
+
+                                    makapasar
+
+                                     
+
+                                   <h1> GOALS</h1>
+                                   <ul>
+                                    <li>Greatly recognize the need of achieving orderliness, discipline and respect among its internal and external stakeholders in the attainment of the harmonious interpersonal relationship and spirit of oneness.</li>
+                                    <li>Highly adhere to its existing policies and standards, rules and regulations as to promote a culture of discipline, i.e.., discipline people, discipline thoughts and discipline actions.</li>
+                                    <li>Profoundly advance quality and excellence in its academic and technopreneurial programs through national and international accreditation.</li>
+                                    <li>Eminently pursue local and global networking with institutions of higher, Technical – Vocational and basic education.</li>
+                                    <li>Energetically promote national and international affiliations, partnership and joint ventures with other organizations to enhance institutional capabilities and competencies.</li>
+                                    <li>Apply acknowledge and honor excellence among its internal and external stakeholders, institutional partners country-wide and worldwide.</li>
+                                    <li>Deeply cultivate and develop the essentiality and continuous relevant research and extension activities among its personnel and students to promote innovations, inventions and novelties.</li>
+                                    <li>Collectively strive and sustain the establishment of Holy Trinity Academy of Loay as a world-class institution from good to great and built to last.</li>
+                                   </ul>
+                                </p>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+           
